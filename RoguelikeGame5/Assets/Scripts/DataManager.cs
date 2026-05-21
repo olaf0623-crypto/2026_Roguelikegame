6b49c28a46ff459f1b09e798f1c5e2044fa9772a
olaf0623-crypto <olaf0623@g.shingu.ac.kr>
2026-05-21 15:36:33 +0900

    5/21

---
+++ RoguelikeGame5/Assets/Scripts/DataManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DataManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
