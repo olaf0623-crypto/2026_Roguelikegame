72c5b0fc4538f28e01cf5ac85f8f507723b7d2c6
olaf0623-crypto <olaf0623@g.shingu.ac.kr>
2026-06-13 22:52:18 +0900

    [Feat]타이틀화면 구현

---
+++ RoguelikeGame5/Assets/Scripts/TitleManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    public GameObject helpPanel;
+
+    public void GamesStart()
+    {
+        SceneManager.LoadScene("Level_1");
+    }
+
+    public void GameTitle()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+
+    public void OpenHelp()
+    {
+        helpPanel.SetActive(true);
+    }
+
+    public void CloseHelp()
+    {
+        helpPanel.SetActive(false);
+    }
+
+    public void ButtonLog()
+    {
+        Debug.Log("BUTTON CLICKED!");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("게임 종료");
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+}
